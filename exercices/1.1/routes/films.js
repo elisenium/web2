08b97e71ff8619e3fbe838e880f3529b2641c561
elisenium <elise.grelaud@student.vinci.be>
2023-09-19 10:08:17 +0200

    1.1 API : read

---
+++ exercices/1.1/routes/films.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const FILMS = [
+    {
+        id: 1,
+        title: 'Palmer',
+        duration: 110,
+        budget: 6.7,
+        link:'https://m.imdb.com/title/tt6857376',
+    },
+    {
+        id: 2,
+        title: 'Divines',
+        duration: 105,
+        budget: 2.4,
+        link: 'https://m.imdb.com/title/tt4730986',
+    },
+    {
+        id: 3,
+        title: 'Kidnap',
+        duration: 95,
+        budget: 21,
+        link: 'https://m.imdb.com/title/tt1458169/',
+    },
+
+];
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    console.log("GET /films")
+    res.json(FILMS);
+});
+
+module.exports = router;
