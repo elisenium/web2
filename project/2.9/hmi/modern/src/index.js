80a7fcda3da0a83d31eb12e7b92cad47b8bdcbbf
elisenium <elise.grelaud@student.vinci.be>
2023-11-13 03:20:57 +0100

    fix: 2.9 typo

--- project/2.9/hmi/modern/src/index.js
+++ project/2.9/hmi/modern/src/index.js
@@ -48,7 +48,7 @@ function renderAboutPage() {
     <div class="container text-center">
         <div class="row">
             <div class="col">
-            <h3>About e-baron...</h3>
+            <h3>About elisenium...</h3>
 
             <p>She is a computer science student learning JS <3</p>
             </div>
