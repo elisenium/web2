c836988c9119b1e39c7e9d0d81d37a3fadb5e2ae
elisenium <elise.grelaud@student.vinci.be>
2023-11-13 03:20:18 +0100

    2.12 : dynamic page content

---
+++ project/2.12/hmi/modern/src/Components/Pages/AboutPage.js
@@ -0,0 +1,16 @@
+const AboutPage = () => {
+
+  const main = document.querySelector('main');
+  main.innerHTML = `
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+            <h3>About elisenium...</h3>
+
+            <p>She is a computer science student learning JS <3</p>
+            </div>
+        </div>        
+    </div>`;
+};
+
+export default AboutPage;
---
+++ project/2.12/hmi/modern/src/Components/Pages/HomePage.js
@@ -0,0 +1,33 @@
+import barbieMovieImg from '../../img/barbie-the-movie.jpg';
+import theHelpMovieImg from '../../img/the-help.jpg';
+
+const HomePage = () => {
+
+  const homePage = `
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+            <h3>Welcome to myMovies !</h3>
+
+            <p>Here you can find a selection of our favorite movies ; )</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-lg-6">
+            <h4>Barbie The Movie</h1>
+            <img class="img-thumbnail" src="${barbieMovieImg}" alt="Barbie" />
+            </div>
+
+            <div class="col-12 col-lg-6">
+            <h4>La Couleur des Sentiments</h1>
+            <img class="img-thumbnail" src="${theHelpMovieImg}" alt="The Help" />
+            </div>
+        </div>
+    </div>
+    `;
+  const main = document.querySelector('main');
+  main.innerHTML = homePage;
+};
+
+export default HomePage;
