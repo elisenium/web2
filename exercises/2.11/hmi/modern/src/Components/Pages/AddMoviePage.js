c318fc47b9e52d282b1a61dbbfb733ceea9f2777
elisenium <elise.grelaud@student.vinci.be>
2023-11-13 02:02:32 +0100

    2.11.2 : add films

--- exercises/2.11/hmi/modern/src/Components/Pages/AddMoviePage.js
+++ exercises/2.11/hmi/modern/src/Components/Pages/AddMoviePage.js
@@ -1,19 +1,80 @@
 import Navigate from '../Router/Navigate';
+import { addOneMovie } from '../../models/movies';
 
 const AddMoviePage = () => {
-  renderGoBackHomeButton();
-};
+  const addMoviePage = `
+<div class="text-center">
+  <h3>Movies</h3>
 
-function renderGoBackHomeButton() {
+  <p>Here you can find all movies</p>
+
+  <form class="px-5">
+            <div class="mb-3">
+              <label for="title">Enter title</label>
+              <input
+                class="form-control"
+                type="text"
+                name="title"
+                id="title"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="duration">Enter duration (minutes)</label>
+              <input
+                class="form-control"
+                type="number"
+                name="duration"
+                id="duration"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="budget">Enter budget (million)</label>
+              <input
+                class="form-control"
+                type="number"
+                name="budget"
+                id="budget"
+                required
+              />
+            </div>
+            <div class="mb-3">
+              <label for="link">Enter link</label>
+              <input
+                class="form-control"
+                type="url"
+                name="link"
+                id="link"
+                required
+              />
+            </div>
+            <input type="submit" class="btn btn-primary" value="Add Movie" />
+    </form>  
+</div>`;
+
   const main = document.querySelector('main');
-  const submit = document.createElement('input');
-  submit.value = 'Go back to HomePage';
-  submit.className = 'btn btn-secondary mt-3';
-  submit.addEventListener('click', () => {
-    Navigate('/');
-  });
+  main.innerHTML = addMoviePage;
 
-  main.appendChild(submit);
-}
+  const myForm = document.querySelector('form');
+  const title = document.querySelector('#title');
+  const duration = document.querySelector('#duration');
+  const budget = document.querySelector('#budget');
+  const link = document.querySelector('#link');
+
+  myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const movieToBeCreated = {
+      title: title.value,
+      duration: duration.value,
+      budget: budget.value,
+      link: link.value,
+    };
+
+    addOneMovie(movieToBeCreated);
+    Navigate('/movies');
+  });
+};
 
 export default AddMoviePage;
