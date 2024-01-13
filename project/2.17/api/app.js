78fdeb2aa527e9b38dfed9a0589d6b92cc617edf
elisenium <elise.grelaud@student.vinci.be>
2024-01-13 18:28:35 +0100

    2.17 : fix

--- project/2.17/api/app.js
+++ project/2.17/api/app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
-
-const corsOptions = {
-    origin: 'http://localhost:8080',
-};
 
 const filmsRouter = require('./routes/films');
 
@@ -16,6 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/films', cors(corsOptions), filmsRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
